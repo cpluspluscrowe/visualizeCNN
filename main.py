601bac60e831915c7e52a84168880147521fdb28
Chad Crowe <ccrowe@linkedin.com>
2018-12-06 19:35:18 -0600

    getting basic cnn visualizations

--- main.py
+++ main.py
@@ -25,14 +25,31 @@ print(sample_image_path)
 from keras.preprocessing import image
 import numpy as np
 
-img = image.load_img(sample_image_path, target_size=(150, 150))
+img = image.load_img(sample_image_path, target_size=(200,200))
 img_tensor = image.img_to_array(img)
 img_tensor = np.expand_dims(img_tensor, axis=0)
 img_tensor /= 255.
 
 import matplotlib.pyplot as plt
 
-plt.imshow(img_tensor[0])
+#plt.imshow(img_tensor[0])
+#plt.show()
+
+from keras import models
+
+layer_outputs = [layer.get_output_at(1) for layer in model.layers[0:1]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+
+
+activations = activation_model.predict(img_tensor)
+first_layer_activation = activations[0]
+
+import matplotlib.pyplot as plt
+
+plt.matshow(activations[0, :, :,:], cmap='viridis')
 plt.show()
+
+
+
 
 
