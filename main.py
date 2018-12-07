70c4422889efec9370a99be288e729fe90ff0750
Chad Crowe <ccrowe@linkedin.com>
2018-12-06 21:31:50 -0600

    uploading photos created by the visualizer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from keras.models import load_model
 import ntpath
 import os 
+from shutil import copyfile
 
 class Global:
     dir_path = "/Users/ccrowe/Documents/gitrepos/visualizeCNN"#os.path.dirname(os.path.realpath(__file__))
@@ -21,51 +22,43 @@ def getModel():
 class CNN:
     model = getModel()
 
-sample_image_path = os.path.join(Global.dir_path,"magie_hall.jpeg") #Global.images[0]
-print(sample_image_path)
+def getImageActivations(image_path):
+    file = ntpath.basename(image_path)
+    current_dir = Global.dir_path
+    new_folder = file[0:file.find(".")-1]
+    sample_image_path = os.path.join(current_dir,new_folder,file)
+    new_folder_path = os.path.join(current_dir,new_folder)
 
-from keras.preprocessing import image
-import numpy as np
+    print(new_folder_path)
+    if not os.path.exists(new_folder_path):
+        os.mkdir(new_folder_path)
+        copyfile(image_path, os.path.join(new_folder_path,file))
+    from keras.preprocessing import image
+    import numpy as np
 
-img = image.load_img(sample_image_path, target_size=(64,64))
-img_tensor = image.img_to_array(img)
-img_tensor = np.expand_dims(img_tensor, axis=0)
-img_tensor /= 255.
+    img = image.load_img(image_path, target_size=(64,64))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
 
-import matplotlib.pyplot as plt
-
-from keras import models
-
-layer_outputs = [layer.output for layer in CNN.model.layers]
-activation_model = models.Model(inputs=CNN.model.input, outputs=layer_outputs)
-
-
-activations = activation_model.predict(img_tensor)
-for activation_layer_index in range(2):#range(len(activations)):
-    activation_layer = activations[activation_layer_index]
     import matplotlib.pyplot as plt
-    for x in range(activation_layer.shape[3]):
-        plt.matshow(activation_layer[0, :, :,x], cmap='viridis')
 
-        save_path = os.path.join(Global.dir_path,str(activation_layer_index) + "-" + str(x) + "-" + ntpath.basename(sample_image_path))
-        print(save_path)
-        plt.savefig(save_path)
+    from keras import models
 
+    layer_outputs = [layer.output for layer in CNN.model.layers]
+    activation_model = models.Model(inputs=CNN.model.input, outputs=layer_outputs)
 
 
-import sys
-from PIL import Image
-images = map(Image.open, list(map(lambda x: os.path.join(Global.dir_path, x),filter(lambda x: "jpg" in x, os.listdir(Global.dir_path)))))
-widths, heights = zip(*(i.size for i in images))
+    activations = activation_model.predict(img_tensor)
+    for activation_layer_index in range(2):#range(len(activations)):
+        activation_layer = activations[activation_layer_index]
+        import matplotlib.pyplot as plt
+        for x in range(activation_layer.shape[3]):
+            plt.matshow(activation_layer[0, :, :,x], cmap='viridis')
 
-total_width = sum(widths)
-max_height = max(heights)
+            save_path = os.path.join(new_folder_path,str(activation_layer_index) + "-" + str(x) + "-" + ntpath.basename(sample_image_path))
+            print(save_path)
+            plt.savefig(save_path)
 
-new_im = Image.new('RGB', (total_width, max_height))
-
-x_offset = 0
-for im in images:
-  new_im.paste(im, (x_offset,0))
-  x_offset += im.size[0]
-
-new_im.save('test.jpg')
+for sample_image_path in Global.images[1:10]:
+    getImageActivations(sample_image_path)
